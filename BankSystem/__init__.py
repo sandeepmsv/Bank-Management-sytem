f2fef1f033fa688e63112e2c8bbc90286a2a4e0c
sandeep <90399591+sandeepmsv@users.noreply.github.com>
2022-08-03 22:13:06 +0530

    Add files via upload

---
+++ BankSystem/__init__.py
@@ -0,0 +1,13 @@
+"""
+BANK MANAGEMENT SYSTEM
+==========================
+MySQL Based BankManagement System
+
+"""
+
+from BankSystem.database import *
+
+__author__ = 'M.S.V.Sandeep'
+__name__ = 'BankingSystem'
+__github__ = 'https://github.com/sandeepmsv'
+__source_code__ = f'{__github__}/Bank-Management-sytem '
